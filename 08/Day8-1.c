93d80fe90ec1c3cfb3ee450723fee912f98d4b0d
allbombson <allbombson@gmail.com>
2020-12-08 10:20:11 -0800

    Arc if your reading this I wrote your VM

---
+++ 08/Day8-1.c
@@ -0,0 +1,53 @@
+// Arc this is becuase you kept nagging me to write a VM
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	FILE *inputFile;
+	char buffer[4];
+	short acc = 0;
+	unsigned short len = 0;
+	short memoryAddress;
+	// 2K of ROM for our program.
+	unsigned char opROM[1024];
+	short argROM[1024];
+	// Another 1k for keeping track of addresses for loop detection
+	unsigned short table[1024];
+
+	inputFile = fopen("input.txt", "r");
+
+	// Clear garbage in table and load data into ROM
+	memset(table, 0, sizeof(table));
+	while (fscanf(inputFile, "%s %hd ", buffer, &argROM[len]) != -1) {
+		opROM[len] = buffer[0];
+		opROM[len + 1] = 0;
+		len++;
+	}
+
+	// VM/Bytecode time
+	for (memoryAddress = 0; memoryAddress < len;) {
+		if (table[memoryAddress] == 1) {
+			break;
+		}
+		table[memoryAddress] = 1;
+
+		switch (opROM[memoryAddress]) {
+			case 'n':
+				memoryAddress++;
+				break;
+			case 'a':
+				acc = acc + argROM[memoryAddress];
+				memoryAddress++;
+				break;
+			case 'j':
+				memoryAddress = memoryAddress + argROM[memoryAddress];
+				break;
+		}
+		printf("%c %d\n", opROM[memoryAddress], argROM[memoryAddress]);
+	}
+
+	printf("%d\n", acc);
+
+	fclose(inputFile);
+	return 0;
+}
